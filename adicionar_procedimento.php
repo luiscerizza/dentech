8cac9fa8b555d91ff8969c48cfc4f99c94f0aaef
luiscerizza <cerizza57@gmail.com>
2026-08-22 11:26:26 -0300

    aaaa

--- adicionar_procedimento.php
+++ adicionar_procedimento.php
@@ -806,15 +806,15 @@ unset($material);
 
 
     <script>
-        const procedimentoExistente = <?= json_encode(
-                                            $procedimento_existente ?: null,
+        const procedimentoExistente = <?= json_encode($procedimento_existente ?: null,
                                             JSON_UNESCAPED_UNICODE |
                                                 JSON_UNESCAPED_SLASHES |
                                                 JSON_HEX_TAG |
                                                 JSON_HEX_APOS |
                                                 JSON_HEX_AMP |
                                                 JSON_HEX_QUOT
-                                        ) ?>;
+                                        )
+                                        ?>;
 
         const materiaisDisponiveis = <?= json_encode(
                                             $materiais,
