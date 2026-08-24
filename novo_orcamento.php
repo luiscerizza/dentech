6e0362c947f05406b0c84ba91cec5d1e13130cf0
luiscerizza <cerizza57@gmail.com>
2026-08-24 12:24:49 -0300

    aaaa

--- novo_orcamento.php
+++ novo_orcamento.php
@@ -482,17 +482,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="css/variables.css">
     <link rel="stylesheet" href="css/layout.css">
     <link rel="stylesheet" href="css/navbar.css">
-    <link rel="stylesheet" href="css/new_orcamento.css">
+    <link rel="stylesheet" href="css/new_orcamento.css?v=4">
 
     <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 
     <link rel="icon" type="image/png" href="img/icon.PNG">
-
-
-    
-
 </head>
 
 
